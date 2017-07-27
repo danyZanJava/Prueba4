22c0579b7a3eabef6c483cd4552dd1776e8ba068
Daniel Zanandrea <danielzanandrea63@gmail.com>
2017-07-27 10:36:00 -0300

    Primer commit

---
+++ ComparacionEquals==/src/MainEquals.java
@@ -0,0 +1,17 @@
+
+public class MainEquals {
+
+	
+	public static void main(String[] args) {
+		
+		 String c1 = "Hola";
+		 
+		 String c2 = new String(c1);
+		 
+		 System.out.println(c1 +  " == " + c2 + " -> " + (c1 == c2));
+		 
+		 System.out.println(c1  + " equals " + c2 + " -> " + c1);
+
+	}
+
+}
